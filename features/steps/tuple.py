851d6164aa981b7386516e9b01963678b019fc62
Craig Treptow <craig@craigtreptow.com>
2024-02-04 21:13:54 +0000

    task: add and subtract tuples and poiknts

--- features/steps/tuple.py
+++ features/steps/tuple.py
@@ -1,24 +1,50 @@
 # type: ignore
+from behave import *
 
 # from pprint import pprint
 # if output is needed, add `print("\n\n")` at the end
-from behave import *
 import rayz.util as U
 import rayz.tuple_util as TU
 
-@given('a ← tuple({x}, {y}, {z}, {w})')
-def step_impl(context, x, y, z, w):
-    context.a = TU.new_tuple(x=float(x), y=float(y), z=float(z), w=float(w))
+@given('{t} ← tuple({x}, {y}, {z}, {w})')
+def step_impl(context, t, x, y, z, w):
+    new_tuple = TU.new_tuple(x=float(x), y=float(y), z=float(z), w=float(w))
 
-@given('p ← point({x}, {y}, {z})')
-def step_impl(context, x, y, z):
-    context.p = TU.new_point(x=float(x), y=float(y), z=float(z))
+    match t:
+        case 'a':
+            context.a = new_tuple
+        case 'a1':
+            context.a1 = new_tuple
+        case 'a2':
+            context.a2 = new_tuple
+
+@given('{p} ← point({x}, {y}, {z})')
+def step_impl(context, p, x, y, z):
+    new_point = TU.new_point(x=float(x), y=float(y), z=float(z))
+
+    match p:
+        case 'p':
+            context.p = new_point
+        case 'p1':
+            context.p1 = new_point
+        case 'p2':
+            context.p2 = new_point
 
 @given('v ← vector({x}, {y}, {z})')
 def step_impl(context, x, y, z):
     context.v = TU.new_vector(x=float(x), y=float(y), z=float(z))
 
 # THEN
+
+@then('{a} + {b} = tuple({x}, {y}, {z}, {w})')
+def step_impl(context, a, b, x, y, z, w):
+    expected = TU.new_tuple(x=float(x), y=float(y), z=float(z), w=float(w))
+    result = None
+
+    if (a == 'a1' and b == 'a2'):
+        result = TU.add(context.a1, context.a2)
+
+    assert TU.equal(result, expected) is True
 
 @then('v = tuple({x}, {y}, {z}, {w})')
 def step_impl(context, x, y, z, w):
@@ -63,3 +89,13 @@ def step_impl(context, type):
             assert TU.is_point(a) is False
         case 'vector':
             assert TU.is_vector(a) is False
+
+@then('{a} - {b} = vector({x}, {y}, {z})')
+def step_impl(context, a, b, x, y, z):
+    expected = TU.new_vector(x=float(x), y=float(y), z=float(z))
+    result = None
+
+    if (a == 'p1' and b == 'p2'):
+        result = TU.subtract(context.p1, context.p2)
+
+    assert TU.equal(result, expected) is True
--- rayz/tuple_util.py
+++ rayz/tuple_util.py
@@ -2,14 +2,48 @@ import rayz.util as U
 # from pprint import pprint
 # if output is needed, add `print("\n\n")` at the end
 
+def subtract(a: tuple, b: tuple) -> tuple:
+    """
+    Subtracts two tuples.  The second tuple is subtracted from the first.
+    e.g. a - b --> subtract(a, b)
+    """
+    r0 = a[0] - b[0]
+    r1 = a[1] - b[1]
+    r2 = a[2] - b[2]
+    r3 = a[3] - b[3]
+    return new_tuple(x = r0, y = r1, z = r2, w = r3)
+
+def add(a: tuple, b: tuple) -> tuple:
+    """
+    Adds two tuples.
+    e.g. a + b --> add(a, b)
+    """
+    r0 = a[0] + b[0]
+    r1 = a[1] + b[1]
+    r2 = a[2] + b[2]
+    r3 = a[3] + b[3]
+    return new_tuple(x = r0, y = r1, z = r2, w = r3)
+
 def new_tuple(x: float, y: float, z: float, w: float) -> tuple:
-    return (x,y,z,w)
+    """
+    Creates a tuple with the given attributes in (x, y, z, w).
+    e.g. new_tuple(x: 1.0, y: 2.0, z: 3.0, w: 4.0) --> (1.0, 2.0, 3.0 4.0)
+    """
+    return (x, y, z, w)
 
 def new_point(x: float, y: float, z: float) -> tuple:
-    return (x,y,z,1.0)
+    """
+    Creates a point with the given attributes in (x, y, z).
+    e.g. new_point(x: 1.0, y: 2.0, z: 3.0) --> (1.0, 2.0, 3.0, 1.0)
+    """
+    return (x, y, z, 1.0)
 
 def new_vector(x: float, y: float, z: float) -> tuple:
-    return (x,y,z,0.0)
+    """
+    Creates a vector with the given attributes in (x, y, z).
+    e.g. new_vector(x: 1.0, y: 2.0, z: 3.0) --> (1.0, 2.0, 3.0, 0.0)
+    """
+    return (x, y, z, 0.0)
 
 def is_point(t: tuple) -> bool:
     return t[3] == 1.0
@@ -18,4 +52,8 @@ def is_vector(t: tuple) -> bool:
     return t[3] == 0.0
 
 def equal(a: tuple, b: tuple) -> bool:
+    """
+    Compares two tuples for equality, based on the values.
+    e.g. equal(a: (1.0, 2.0, 3.0, 1.0), b: (1.0, 2.0, 3.0, 0.0) --> False
+    """
     return U.equal(a[0], b[0]) and U.equal(a[1], b[1]) and U.equal(a[2], b[2]) and U.equal(a[3], b[3])
