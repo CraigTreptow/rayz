5de7f857ab7b89cd0d7e5940d9e6d10a691a1cf4
Craig Treptow <craig@craigtreptow.com>
2024-02-13 04:23:27 +0000

    test mypy CI

--- rayz/util.py
+++ rayz/util.py
@@ -3,6 +3,6 @@ class ColumnIndexTooLarge(Exception):
 class RowIndexTooLarge(Exception):
     pass
 
-def equal(a: float, b: float) -> bool:
+def equal(a: float, b: float) -> float:
     EPSILON = 0.00001
     return abs(a - b) < EPSILON
