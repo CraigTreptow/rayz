d90963a72d80cd8a35ffb125620e49ccd9874256
Craig Treptow <craig@craigtreptow.com>
2024-02-13 04:25:08 +0000

    task: combined linting action

--- rayz/util.py
+++ rayz/util.py
@@ -3,6 +3,6 @@ class ColumnIndexTooLarge(Exception):
 class RowIndexTooLarge(Exception):
     pass
 
-def equal(a: float, b: float) -> float:
+def equal(a: float, b: float) -> int:
     EPSILON = 0.00001
     return abs(a - b) < EPSILON
