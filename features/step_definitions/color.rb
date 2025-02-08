f24e1faf2c74bc65bb511754447a814386169863
CraigTreptow <craig.treptow@gmail.com>
2025-02-08 17:04:33 -0600

    add color operations

---
+++ features/step_definitions/color.rb
@@ -0,0 +1,43 @@
+Given('c ← color\({float}, {float}, {float})') do |float, float2, float3|
+  @c = Color.new(red: float, green: float2, blue: float3)
+end
+
+Then("c.red = {float}") do |float|
+  assert_equal(@c.red, float)
+end
+
+Then("c.green = {float}") do |float|
+  assert_equal(@c.green, float)
+end
+
+Then("c.blue = {float}") do |float|
+  assert_equal(@c.blue, float)
+end
+
+Given('c1 ← color\({float}, {float}, {float})') do |float, float2, float3|
+  @c1 = Color.new(red: float, green: float2, blue: float3)
+end
+
+Given('c2 ← color\({float}, {float}, {float})') do |float, float2, float3|
+  @c2 = Color.new(red: float, green: float2, blue: float3)
+end
+
+Then('c1 + c2 = color\({float}, {float}, {float})') do |float, float2, float3|
+  expected = Color.new(red: float, green: float2, blue: float3)
+  assert_equal(@c1 + @c2, expected)
+end
+
+Then('c1 - c2 = color\({float}, {float}, {float})') do |float, float2, float3|
+  expected = Color.new(red: float, green: float2, blue: float3)
+  assert_equal(@c1 - @c2, expected)
+end
+
+Then('c1 * c2 = color\({float}, {float}, {float})') do |float, float2, float3|
+  expected = Color.new(red: float, green: float2, blue: float3)
+  assert_equal(@c1 * @c2, expected)
+end
+
+Then('c * {float} = color\({float}, {float}, {float})') do |float, float2, float3, float4|
+  expected = Color.new(red: float2, green: float3, blue: float4)
+  assert_equal(@c * float, expected)
+end
