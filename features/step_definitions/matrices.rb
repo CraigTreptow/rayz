c199186ebc7dc9d9103fe040397d1a1dcccfb286
CraigTreptow <craig.treptow@gmail.com>
2025-02-14 13:31:09 -0600

    add more matrix operations

--- features/step_definitions/matrices.rb
+++ features/step_definitions/matrices.rb
@@ -5,6 +5,34 @@ Given("the following {} matrix M:") do |size, table|
   @m = Matrix[*table_values.map { |row| row.map { |x| x.to_f } }]
 end
 
+Given("the following matrix A:") do |table|
+  table_values = table.raw
+  @m_a = Matrix[*table_values.map { |row| row.map { |x| x.to_f } }]
+end
+
+Given("the following matrix B:") do |table|
+  table_values = table.raw
+  @m_b = Matrix[*table_values.map { |row| row.map { |x| x.to_f } }]
+end
+
+Then("A = B") do
+  assert_equal(@m_a, @m_b)
+end
+
+Then("A != B") do
+  refute_equal(@m_a, @m_b)
+end
+
+Then("A * B") do
+  refute_equal(@m_a, @m_b)
+end
+
+Then('A * B is the following {} matrix:') do |size, table|
+  table_values = table.raw
+  expected = Matrix[*table_values.map { |row| row.map { |x| x.to_f } }]
+  assert_equal(@m_a * @m_b, expected)
+end
+
 Then("M[{int},{int}] = {}") do |int, int2, val|
   m_val = @m[int, int2]
   assert_equal(m_val, val.to_f)
